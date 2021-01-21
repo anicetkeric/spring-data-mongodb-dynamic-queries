ba4fe78548a27b9c5a3029ada384d025c2a485ab
aek <konan.kouame@soprasteria.com>
2021-01-21 10:50:21 +0100

    initial commit

---
+++ src/main/java/com/tutorial/springdatamongodbdynamicqueries/SpringDataMongodbDynamicQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.springdatamongodbdynamicqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMongodbDynamicQueriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataMongodbDynamicQueriesApplication.class, args);
+    }
+
+}
